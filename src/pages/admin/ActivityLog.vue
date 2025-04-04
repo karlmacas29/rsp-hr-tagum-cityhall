81ba7d2d3b7c4c6f44394cd2fa0670c9b17d83e5
karlmacas29 <karlracomamacas@gmail.com>
2025-04-04 16:55:26 +0800

    modified:   src/pages/admin/ActivityLog.vue

--- src/pages/admin/ActivityLog.vue
+++ src/pages/admin/ActivityLog.vue
@@ -1,5 +1,13 @@
 <template>
   <q-page class="q-pa-md">
+    <div class="column items-start justify-center q-mb-md">
+      <h5 class="text-h4 q-ma-none"><b>Activity Log</b></h5>
+      <div class="q-pa-md q-gutter-sm">
+        <q-breadcrumbs class="q-ma-none">
+          <q-breadcrumbs-el class="text-bold" label="Admins Log" />
+        </q-breadcrumbs>
+      </div>
+    </div>
     <q-card>
       <q-card-section>
         <div class="text-h6">Activity Log</div>

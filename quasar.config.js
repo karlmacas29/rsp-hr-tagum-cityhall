e65111a54d70200c42021b40493b30c8782bee25
karlmacas29 <karlracomamacas@gmail.com>
2025-04-04 15:50:06 +0800

    modified:   quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -99,7 +99,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['LoadingBar', 'Loading', 'Notify', 'Dialog'],
+      plugins: ['Loading', 'Notify', 'Dialog'],
     },
 
     // animations: 'all', // --- includes all animations
